af26475e73494d46d9b5ab371b2c4a89dc9ff7c0
vvssgowtham <gowthamvvss@gmail.com>
2024-01-27 01:34:03 +0530

    HomePage Completed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,11 @@ import React from "react";
 import "../css/Home.css";
 import { MdDeliveryDining } from "react-icons/md";
 import { useState } from "react";
+import popular from "../utils/data";
+
+import { AiFillInstagram } from "react-icons/ai";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 const Home = () => {
   const [login, setLogin] = useState(false);
@@ -14,8 +19,8 @@ const Home = () => {
               style={{ fontSize: "2rem", marginRight: "1rem" }}
             />
             <a href="/">Home</a>
-            <a href="/services">Services</a>
-            <a href="/contact">Contact</a>
+            <a href="#part2">Yummy</a>
+            <a href="#part3">About</a>
           </div>
           <div>
             <input type="text" placeholder="Search.." />
@@ -36,8 +41,9 @@ const Home = () => {
           <div className="content">
             <h1>Food Delivery</h1>
             <p>
-            Indulge in the Culinary Delights at Your Doorstep!<br/>
-            Elevate Your Tastebuds with Our Seamless Food Delivery Experience.
+              Indulge in the Culinary Delights at Your Doorstep!
+              <br />
+              Elevate Your Tastebuds with Our Seamless Food Delivery Experience.
             </p>
             <button>Order Now</button>
           </div>
@@ -45,7 +51,66 @@ const Home = () => {
             <img src="ScooterRide.gif" />
           </div>
         </div>
+        <hr />
+        <div className="section2" id="part2">
+          <div className="popular">
+            <h1>
+              <u>Popular Dishes</u>
+            </h1>
+            <p>
+              "Savor the Flavor, Embrace the Popular! Explore a Culinary
+              Symphony with Our Signature Dishes – Where Every Bite Tells a
+              Delicious Tale."
+            </p>
+          </div>
+
+          <div className="cards">
+            {popular.map((item) => {
+              return (
+                <div className="card" key={item.id}>
+                  <div className="card-image">
+                    <img src={item.image} />
+                  </div>
+                  <div className="card-text">
+                    <h2>
+                      <a href="/food">{item.title}</a>
+                    </h2>
+                    <p>{item.price}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </header>
+      <hr />
+      <footer id="part3">
+        <div className="footerContainer">
+          <div className="popular">
+            <h1>
+              <u>Lightening Fast</u>
+            </h1>
+            <p>
+              "Indulge in the Culinary Delights at Your Doorstep! Elevate Your
+              Tastebuds with Our Seamless Food Delivery Experience."
+            </p>
+          </div>
+          <div className="footerbox">
+            <div className="social">
+              <FaXTwitter />
+              <a href="https://twitter.com/vvssgowtham">Twitter</a>
+            </div>
+            <div className="social">
+              <FaLinkedin />
+              <a href="https://www.linkedin.com/in/vvssgowtham/">LinkedIn</a>
+            </div>
+            <div className="social">
+              <AiFillInstagram />
+              <a href="https://www.instagram.com/i.am_vvss/">Instagram</a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
---
+++ client/src/utils/data.js
@@ -0,0 +1,46 @@
+const popular = [
+    {
+        id: 1,
+        title: "Burger's",
+        price: "Rs. 150",
+        image: "logo192.png",
+    },
+    {
+        id: 2,
+        title: "Pizza's",
+        price: "Rs. 250",
+        image: "logo192.png",
+    },
+    {
+        id: 3,
+        title: "Sandwich",
+        price: "Rs. 100",
+        image: "logo192.png",
+    },
+    {
+        id: 4,
+        title: "Pasta",
+        price: "Rs. 200",
+        image: "logo192.png",
+    },
+    {
+        id: 5,
+        title: "Noodles",
+        price: "Rs. 150",
+        image: "logo192.png",
+    },
+    {
+        id: 6,
+        title: "Fries",
+        price: "Rs. 100",
+        image: "logo192.png",
+    },
+    {
+        id: 7,
+        title: "Drinks",
+        price: "Rs. 50",
+        image: "logo192.png",
+    }
+]
+
+export default popular;
